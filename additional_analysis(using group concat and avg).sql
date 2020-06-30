172521a7f663001ababd1ea7b44debf0c5fe8960
Akanksha Bakshi <akankshabakshi123@gmail.com>
2020-06-30 21:29:28 +0530

    Update additional_analysis(using group concat and avg).sql

--- additional_analysis(using group concat and avg).sql
+++ additional_analysis(using group concat and avg).sql
@@ -1,15 +1,15 @@
-# To check if one npi - national provider identifier can have multiple names or not using group_concat function to concancenate it in the form of array and see results in one row
-#For querying purposes, we will take a small set of facilities which we can differenciate using entity type as "O"
+----- To check if one npi - national provider identifier can have multiple names or not using group_concat function to concancenate it in the form of array and see results in one row
+----For querying purposes, we will take a small set of facilities which we can differenciate using entity type as "O"
 
 select NPI, group_concat(distinct Facilityname), count(*) as count
 from CMS
 where type = 'O'
 group by NPI;
 
-#After runnning the query to "having count>1" it returned no results which means one NPI is having a facility name. That is a good news and it is coming as expected
+----After runnning the query to "having count>1" it returned no results which means one NPI is having a facility name. That is a good news and it is coming as expected
 
 
-#Also, I was looking into primary fields NPI and HCPCSCode but there is a problem the same values is returning multiple rows due to field - place of service which contains facility setting or freestanding physicians office and corresponding field - prices is coming multiple times. In order to eliminate multiple rows issue and mark NPI and HCPCSCode as primary field we would need to group by the primary fields and take average of prices to resolve this
+----Also, I was looking into primary fields NPI and HCPCSCode but there is a problem the same values is returning multiple rows due to field - place of service which contains facility setting or freestanding physicians office and corresponding field - prices is coming multiple times. In order to eliminate multiple rows issue and mark NPI and HCPCSCode as primary field we would need to group by the primary fields and take average of prices to resolve this
 
 create table CMS_2017_table as 
 select 
