4eb7093897ff49bfdc9e1738a5457734476d41de
Akanksha Bakshi <akankshabakshi123@gmail.com>
2020-07-03 15:39:08 +0530

    Update additional_analysis(using group concat and avg).sql

--- additional_analysis(using group concat and avg).sql
+++ additional_analysis(using group concat and avg).sql
@@ -5,9 +5,15 @@ select NPI, group_concat(distinct Facilityname), count(*) as count
 from CMS
 where type = 'O'
 group by NPI;
+---type 'O' indicates that the NPI is a facility and count above would return the count of times those NPI rows have repeated, not exactly the different names count as 1
+
+---To find the NPI having multiple names
+select NPI, group_concat(distinct Facilityname), count(distinct n.Facilityname) as count
+from CMS
+where type = 'O'
+group by NPI;
 
 ----After runnning the query to "having count>1" it returned no results which means one NPI is having a facility name. That is a good news and it is coming as expected
-
 
 ----Also, I was looking into primary fields NPI and HCPCSCode but there is a problem the same values is returning multiple rows due to field - place of service which contains facility setting or freestanding physicians office and corresponding field - prices is coming multiple times. In order to eliminate multiple rows issue and mark NPI and HCPCSCode as primary field we would need to group by the primary fields and take average of prices to resolve this
 
