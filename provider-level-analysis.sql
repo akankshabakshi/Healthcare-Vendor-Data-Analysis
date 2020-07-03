c315dcdc521a2e141896232db21f2416a8d04661
Akanksha Bakshi <akankshabakshi123@gmail.com>
2020-07-03 22:03:07 +0530

    Update provider-level-analysis.sql

--- provider-level-analysis.sql
+++ provider-level-analysis.sql
@@ -33,4 +33,20 @@ from doctor d
 inner join facility f 
 on d.address1=f.address1 and d.address2=f.address2 and d.st = f.st and d.countrycode = f.countrycode and d.zip = f.zip and d.code = f.code;
 
+----let's create a materialized view of this structure
 
+CREATE VIEW procedure_wise_providers AS
+WITH doctor as 
+( 
+select cms_npi, address1, address2, st, countrycode, zip, code from CMS_2017
+where type = 'I' and Drugindicator = 'N'
+),
+facility as 
+(
+select cms_npi, address1, address2, st, countrycode, zip, code from CMS_2017
+where type = 'O' and Drugindicator = 'N'
+)
+select distinct d.cms_npi as doctor_npi, f.cms_npi as facility_npi, d.address1, d.address2, d.st, d.countrycode, d.zip, d.code as code
+from doctor d
+inner join facility f 
+on d.address1=f.address1 and d.address2=f.address2 and d.st = f.st and d.countrycode = f.countrycode and d.zip = f.zip and d.code = f.code;
